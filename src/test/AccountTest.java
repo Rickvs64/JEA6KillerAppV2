38254be8aad8e920d3b58d7bd33021c48e3553c2
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-05-02 16:07:18 +0200

    Add AddAccount test method

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,6 +1,13 @@
+import Domains.Account;
+import Domains.EPlatform;
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class AccountTest extends FunctionalTest{
@@ -12,13 +19,20 @@ public class AccountTest extends FunctionalTest{
 
     @Test
     public void incorrectUrl() {
-        given().log().all().when().get("/account/test").then()
+        given()
+                .log().all()
+                .when()
+                .get("/account/test").then()
                 .statusCode(404);
     }
 
     @Test
     public void getAccountById() {
-        given().log().all().when().get("/account/1").then()
+        given()
+                .log().all()
+                .when()
+                .get("/account/1")
+                .then()
                 .statusCode(200)
                 .assertThat()
                 .body("result.id", equalTo(1));
@@ -26,16 +40,28 @@ public class AccountTest extends FunctionalTest{
 
     @Test
     public void getAllAccounts() {
-        // Not yet implemented!
-
-        //      given().when().get("/rest/account").then()
-        //      .statusCode(200)
-        //      .assertThat()
-        //      ...?
+        given()
+                .log().all()
+                .when()
+                .get("/account")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body("result", hasSize(greaterThan(0)));
     }
 
     @Test
     public void addAccount() {
-        // Not yet implemented!
+        String unique = "RestAssured_" + UUID.randomUUID().toString().substring(0, 9);
+        Account account = new Account(unique, EPlatform.PC);
+
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body(account)
+                .when().post("/account").then()
+                .statusCode(200)
+                .assertThat()
+                .body("result", equalTo(account.getName() + " has been added."));
     }
 }
